af9c9d68c43ef836cec79f3b28a7eec25cbbeb44
senyumpanda <72228596+senyumpanda@users.noreply.github.com>
2021-08-16 01:17:23 +0700

    Create seven_six.cpp

---
+++ seven_six.cpp
@@ -0,0 +1,452 @@
+#include <iostream>
+#include <Windows.h>
+#include <stdlib.h>
+
+void manipulate(int x = 0, int y = 0){
+    COORD poin;
+    poin.X = x;
+    poin.Y = y;
+
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), poin);
+};
+
+void delay(){
+    int delay;
+	delay = 5;
+	while(delay<10000000){
+	delay++;}
+};
+
+void border(){
+    system("color F4");
+    for(int i = 0; i < 60; i++){
+        manipulate(i+8,0);
+        std::cout << '=';
+        delay();
+    }
+
+    for(int i = 0; i < 60; i++){
+        manipulate(i+ 8,18);
+        std::cout << '=';
+        delay();
+    }
+};
+
+void seven(){
+    manipulate(8, 2);
+    for(int i = 0; i < 20; i++){
+        if(i == 0 | i == 1 | i == 2 |i == 19){
+            std::cout << ' ';
+            delay();
+        }else{
+            std::cout << '*';
+            delay();
+        }
+
+    }
+    std::cout << std::endl;
+
+    manipulate(8, 3);
+    for(int i = 0; i < 20; i++){
+        if(i == 0 | i == 1 | i == 2 | i == 3 |i == 18 | i == 19){
+            std::cout << ' ';
+            delay();
+        }
+        else{
+            std::cout << '*';
+            delay();
+        }
+    }
+    std::cout << std::endl;
+
+    manipulate(8, 4);
+    for(int i = 0; i < 20; i++){
+        if(i >= 15 && i <= 16){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    }
+
+    manipulate(8, 5);
+    for(int i = 0; i < 20; i++){
+        if(i >= 11 && i <= 15){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    }
+
+    manipulate(8, 6);
+    for(int i = 0; i < 20; i++){
+        if(i >= 10 && i <= 14){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    }
+
+    manipulate(8, 7);
+    for(int i = 0; i < 20; i++){
+        if(i >= 9 && i <= 13){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    }
+
+    manipulate(8, 8);
+    for(int i = 0; i < 20; i++){
+        if(i >= 8 && i <= 12){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    }
+
+    manipulate(8, 9);
+    for(int i = 0; i < 20; i++){
+        if(i >= 7 && i <= 10){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    }
+
+    manipulate(8, 10);
+    for(int i = 0; i < 20; i++){
+        if(i >= 6 && i <= 9){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    }
+
+    manipulate(8, 11);
+    for(int i = 0; i < 20; i++){
+        if(i >= 5 && i <= 7){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    }
+
+};
+
+void six1(){
+    manipulate(24, 6);
+    for(int i = 0; i < 20; i++){
+        if(i > 3 && i < 6){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    };
+    std::cout << std::endl;
+
+    manipulate(24, 7);
+    for(int i = 0; i < 10; i++){
+        if(i > 2 && i <= 6){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    };
+    std::cout << std::endl;
+
+
+    manipulate(24, 8);
+    for(int i = 0; i < 10; i++){
+        if(i > 1 && i <= 6){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    };
+    std::cout << std::endl;
+
+
+    manipulate(24, 9);
+    for(int i = 0; i < 10; i++){
+        if(i > 0 && i <= 5){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    };
+    std::cout << std::endl;
+
+
+    manipulate(24, 10);
+    for(int i = 0; i < 10; i++){
+        if(i >= 0 && i < 2){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    };
+    std::cout << std::endl;
+};
+
+void six2(){
+
+    manipulate(21, 11);
+    for(int i = 0; i < 20; i++){
+        if(i > 1 && i < 10){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    };
+    std::cout << std::endl;
+
+    manipulate(21, 12);
+    for(int i = 0; i < 20; i++){
+        if(i >= 1 && i < 11){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    };
+    std::cout << std::endl;
+
+    manipulate(21, 13);
+    for(int i = 0; i < 20; i++){
+        if((i >= 0 && i < 4) | (i > 7 && i < 12)){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    };
+    std::cout << std::endl;
+
+    manipulate(21, 14);
+    for(int i = 0; i < 20; i++){
+        if((i >= 0 && i < 4) | (i > 7 && i < 12)){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    };
+    std::cout << std::endl;
+
+    manipulate(21, 15);
+    for(int i = 0; i < 20; i++){
+        if(i >= 1 && i < 11){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    };
+    std::cout << std::endl;
+
+    manipulate(21, 16);
+    for(int i = 0; i < 20; i++){
+        if(i > 1 && i < 10){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    };
+    std::cout << std::endl;
+
+};
+
+void t(){
+
+    manipulate(28,2);
+    for(int i = 0; i < 8; i++){
+        if(i > 2 && i < 6){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    }
+    std::cout << std::endl;
+
+
+    manipulate(28,3);
+    for(int i = 0; i < 8; i++){
+        if(i == 4){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    }
+    std::cout << std::endl;
+
+
+    manipulate(28,4);
+    for(int i = 0; i < 8; i++){
+        if(i == 4){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    }
+    std::cout << std::endl;
+
+};
+
+void h(){
+
+    manipulate(36,2);
+    for(int i = 0; i < 10; i++){
+        if(i == 0 | i == 3){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    }
+    std::cout << std::endl;
+
+    manipulate(36,3);
+    for(int i = 0; i < 10; i++){
+        if(i <= 3){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    }
+    std::cout << std::endl;
+
+    manipulate(36,4);
+    for(int i = 0; i < 10; i++){
+        if(i == 0 | i == 3){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    }
+    std::cout << std::endl;
+
+};
+
+void flag(){
+
+    manipulate(50, 7);
+    for(int i = 0; i < 15; i++){
+        std::cout << '*';
+        delay();
+    }
+    std::cout << std::endl;
+
+    manipulate(50, 8);
+    for(int i = 0; i < 15; i++){
+        std::cout << '*';
+        delay();
+    }
+    std::cout << std::endl;
+
+    manipulate(50, 9);
+    for(int i = 0; i < 15; i++){
+        std::cout << '*';
+        delay();
+    }
+    std::cout << std::endl;
+
+    manipulate(50, 10);
+    for(int i = 0; i < 15; i++){
+        if(i == 0 | i == 14){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    }
+    std::cout << std::endl;
+
+    manipulate(50, 11);
+    for(int i = 0; i < 15; i++){
+        if(i == 0 | i == 14){
+            std::cout << '*';
+            delay();
+        }else{
+            std::cout << ' ';
+            delay();
+        }
+    }
+    std::cout << std::endl;
+
+    manipulate(50, 12);
+    for(int i = 0; i < 15; i++){
+        std::cout << '*';
+        delay();
+    }
+    std::cout << std::endl;
+};
+
+int main(){
+
+    border();
+    seven();
+    six1();
+    six2();
+    t();
+    h();
+    flag();
+
+    std::cout << "\n\n\n\n\n\n\n";
+    manipulate(20, 20);
+    std::string kata = "Source Code on my GitHub: @senyumpanda";
+    for(int i = 0; i < kata.size(); i++){
+        delay();
+        std::cout << kata[i];
+    }
+    std::cout << std::endl;
+    manipulate(35, 21);
+    std::cout << "***" << std::endl;
+    
+  return 0;
+}
